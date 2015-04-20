365e99e97a74c38b233037157955aa840b328c73
Micah Hawkins <micahbhawkins@gmail.com>
2015-04-19 17:23:58 -0700

    Still just experimenting

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+// Placed the map into the global namespace for now
+// This makes it easier to experiment with its API
+var map;
+
 function initialize() {
 	var mapOptions = {
   		center: { lat: 45.583234, lng: -122.733041},
-  		zoom: 1
+  		zoom: 11,
+  		mapTypeId: google.maps.MapTypeId.HYBRID
 	};
 
-	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
+
+var viewModel {
+	points: ko.observableArray();
+};
 
 google.maps.event.addDomListener(window, 'load', initialize);
