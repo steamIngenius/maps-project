58237b250c20b9fbd8e08d3533a650bce13f48bb
Micah Hawkins <micahbhawkins@gmail.com>
2015-04-19 17:54:29 -0700

    Beginning knockout.js/Gooble Maps - no markers yet

--- app.js
+++ app.js
@@ -1,19 +1,51 @@
+// referenced http://jsfiddle.net/t9wcC/
+function point(name, lat, long) {
+    this.name = name;
+    this.lat = ko.observable(lat);
+    this.long = ko.observable(long);
+
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(lat, long),
+        title: name,
+        map: map, // global namespace
+        draggable: true
+    });
+
+    //if you need the poition while dragging
+    google.maps.event.addListener(marker, 'drag', function() {
+        var pos = marker.getPosition();
+        this.lat(pos.lat());
+        this.long(pos.lng());
+    }.bind(this));
+
+    //if you just need to update it when the user is done dragging
+    google.maps.event.addListener(marker, 'dragend', function() {
+        var pos = marker.getPosition();
+        this.lat(pos.lat());
+        this.long(pos.lng());
+    }.bind(this));
+}
+
 // Placed the map into the global namespace for now
 // This makes it easier to experiment with its API
 var map;
 
 function initialize() {
 	var mapOptions = {
-  		center: { lat: 45.583234, lng: -122.733041},
-  		zoom: 11,
-  		mapTypeId: google.maps.MapTypeId.HYBRID
+  		center: { lat: 45.578766, lng: -122.724023},
+  		zoom: 15,
+  		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+
+	var viewModel = {
+		points: ko.observableArray([
+			new point('Western Meats', 45.579660, -122.715667),
+			new point('McKenna Park', 45.581673, -122.733106),
+			new point('Cha Cha Cha', 45.581943, -122.722083) ])
 	};
 
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	ko.applyBindings(viewModel);
 }
-
-var viewModel {
-	points: ko.observableArray();
-};
 
 google.maps.event.addDomListener(window, 'load', initialize);
