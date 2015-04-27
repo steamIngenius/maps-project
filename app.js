a9a040d621fff397069842ea6c385f77f6c38af1
Micah Hawkins <micahbhawkins@gmail.com>
2015-04-26 22:38:40 -0700

    Working search pins, bounds are still wonky

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // referenced http://jsfiddle.net/t9wcC/
-function point(name, lat, long) {
+function point(name, lat, lng) {
     this.name = name;
     this.lat = ko.observable(lat);
-    this.long = ko.observable(long);
+    this.lng = ko.observable(lng);
 
     var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(lat, long),
+        position: new google.maps.LatLng(lat, lng),
         title: name,
         map: map, // global namespace
         draggable: true
@@ -15,19 +15,19 @@ function point(name, lat, long) {
     google.maps.event.addListener(marker, 'drag', function() {
         var pos = marker.getPosition();
         this.lat(pos.lat());
-        this.long(pos.lng());
+        this.lng(pos.lng());
     }.bind(this));
 
     //if you just need to update it when the user is done dragging
     google.maps.event.addListener(marker, 'dragend', function() {
         var pos = marker.getPosition();
         this.lat(pos.lat());
-        this.long(pos.lng());
+        this.lng(pos.lng());
     }.bind(this));
 }
 
 // Placed the map into the global namespace for now
-// This makes it easier to experiment with its API
+// This makes it easier to experiment with its API from the console
 var map;
 
 function initialize() {
@@ -49,12 +49,60 @@ function initialize() {
 	ko.applyBindings(viewModel);
 
 	// Searchbox functionality
-	// TODO: replace with jQuery at some point
+	// TODO: replace with jQuery at some point?
+	// referenced: https://developers.google.com/maps/documentation/javascript/examples/places-searchbox
 	var input = document.getElementById('searchbox');
 	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
 	// Tie the input to the searchbox
 	var searchBox = new google.maps.places.SearchBox(input);
+
+
+	google.maps.event.addListener(searchBox, 'places_changed', function() {
+		var places = searchBox.getPlaces();
+		console.log("List of places retrieved.");
+
+		console.log(places.length);
+		console.log(places[0]);
+
+		if (places.length == 0) { return; }
+
+		var bounds = new google.maps.LatLngBounds();
+	    for (var i = 0, place; place = places[i]; i++) {
+	      var image = {
+	        url: place.icon,
+	        size: new google.maps.Size(71, 71),
+	        origin: new google.maps.Point(0, 0),
+	        anchor: new google.maps.Point(17, 34),
+	        scaledSize: new google.maps.Size(25, 25)
+	      };
+
+	      // Create a marker for each place.
+	      var marker = new google.maps.Marker({
+	        map: map,
+	        icon: image,
+	        title: place.name,
+	        position: place.geometry.location
+	      });
+
+	      // markers.push(marker);
+	      // viewModel.points.push(marker);
+	      console.log(marker);
+	      viewModel.points.push(new point(marker.title, marker.position.k, marker.position.D));
+
+	      bounds.extend(place.geometry.location);
+	    }
+
+	    map.fitBounds(bounds);
+	});
+
+	// Bias the SearchBox results towards places that are within the bounds of the
+  	// current map's viewport.
+  	google.maps.event.addListener(map, 'bounds_changed', function() {
+    	var bounds = map.getBounds();
+    	searchBox.setBounds(bounds);
+    	console.log("Bounds updated.");
+  	});
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
